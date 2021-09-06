35784a15378e078a0f9d48fdd4c0ab6bc2fb0388
Aaron Schneider <contact@aaronschneider.org>
2021-09-05 22:39:10 -0400

    Update App.js
    test branches

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Testing
+         Testing out branches.
         </p>
         <a
           className="App-link"
